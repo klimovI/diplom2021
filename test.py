e2651a27ae7b8e356c37e1be8ce61bb66b56fcb3
KlimovI <irracli@gmail.com>
2021-04-29 18:48:25 +0800

    test

--- test.py
+++ test.py
@@ -4,20 +4,41 @@ from scipy.interpolate import interp1d
 from math import sin, cos, exp, radians, acos, pi, ceil
 
 
+a = 7
 x = []
 y = []
-for i in range(0, 3):
-  xi = i * pi / 4
+for i in range(0, a + 1):
+  xi = i * pi / (a + 2)
   yi = sin(xi)
   x.append(xi)
   y.append(yi)
 
+
+print(x)
+
 finterp1d = interp1d(x, y, kind='quadratic')
 
 f = spline(x, y)
+
 x = pi / 5
+print(a)
+print('pi / 5')
+f1 = f(x)
+print('f', round(f1, 3))
+s1 = sin(x)
+print('s', round(s1, 3))
+abs1 = abs(f(x) - sin(x))
+print('abs', round(abs1, 3))
+o1 = abs1 / s1
+print('o', round(o1 * 100, 3))
 
-print(f(x))
-print(sin(x))
-print(finterp1d(x))
-
+x = pi / 3
+print('pi / 3')
+f1 = f(x)
+print('f', round(f1, 3))
+s1 = sin(x)
+print('s', round(s1, 3))
+abs1 = abs(f(x) - sin(x))
+print('abs', round(abs1, 3))
+o1 = abs1 / s1
+print('o', round(o1 * 100, 3))
